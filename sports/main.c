e392f6ac287e6ab82628b26059fa6ae054b9e118
eunmin1015 <jangeunmin38@gmail.com>
2022-12-09 17:16:38 +0900

    좌표 확인용 코드

---
+++ sports/main.c
@@ -0,0 +1,38 @@
+#include <getopt.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h> // for open/close
+#include <fcntl.h> // for O_RDWR//
+#include <sys/ioctl.h> // for ioctl
+#include <sys/msg.h>
+#include <pthread.h>
+#include <linux/fb.h>
+
+
+#include "led.h"
+#include "fnd.h"
+#include "acc.h"
+#include "button.h"
+#include "buzzer.h"
+#include "textlcd.h"
+#include "colorLed.h"
+#include "Temperature.h"
+#include "bitmap.h"
+#include "embe.h"
+#include "touch.h"
+
+int main(int argc, char* argv[]) {
+   
+	setInit(); //각종 기기들을 키고 log.txt 파일을 오픈하며 초기 설정을 해준다.
+	
+	bitmainfunc("gamestart.bmp"); //메인 메뉴 사진을 띄운다.
+
+    touchInit();
+    while(1);
+
+}
